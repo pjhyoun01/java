aa4a7b15b4867eed80514aa9a07112274f136e5d
pjhyoun01 <pjhyoun01@gmail.com>
2026-01-26 20:54:32 +0900

    260126 HW

--- java_exam/src/exam/baekjoon/Q2743.java
+++ java_exam/src/exam/baekjoon/Q2743.java
@@ -7,6 +7,14 @@ public class Q2743 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 
+//		no. 27866
+//		System.out.println("단어를 입력해주세요: ");
+//		String inputWord = sc.nextLine();
+//		System.out.println("숫자를 입력해주세요: ");
+//		int findNum = sc.nextInt();
+//		
+//		System.out.println(inputWord.charAt(findNum));
+
 //		no. 2743
 //		System.out.println("단어를 입력해주세요: ");
 //		String inputword = sc.nextLine();
@@ -42,34 +50,99 @@ public class Q2743 {
 //		System.out.println(strArray.length);
 
 //		no. 2908
-		System.out.println("숫자 3자리를 입력하세요: ");
-		String strNum1 = sc.nextLine();
-		String strNum2 = sc.nextLine();
-
-		/*
-		 * 숫자 문자열을 입력 받아서 각각 배열에 저장
-		 */
-		int num1;
-		int num2;
-		String[] numArray1 = new String[3];
-		String[] numArray2 = new String[3];
-		String concatNum1 = null;
-		String concatNum2 = null;
-		for (int i = 0; i < strNum1.length(); i++) {
-			numArray1[i] = strNum1.charAt(i) + "";
-			numArray2[i] = strNum2.charAt(i) + "";
-		}
-		for (int i = 2; i <= 0; i--) {
-			concatNum1.concat(numArray1[i]);
-			concatNum2.concat(numArray2[i]);
-
-		}
+//		System.out.println("숫자를 입력하세요: ");
+//		String strNum1 = sc.nextLine();
+//		String strNum2 = sc.nextLine();
+//
+//		String[] numArray1 = strNum1.split("");
+//		String[] numArray2 = strNum2.split("");
+//
+//		String concatNum1 = "";
+//		String concatNum2 = "";
+//		for (int i = numArray1.length - 1; i >= 0; i--) {
+//
+//			concatNum1 += Integer.parseInt(numArray1[i].charAt(0) + "");
+//			concatNum2 += Integer.parseInt(numArray2[i].charAt(0) + "");
+//		}
 //		if (Integer.parseInt(concatNum1) > Integer.parseInt(concatNum2)) {
 //			System.out.println(concatNum1);
 //		} else {
 //			System.out.println(concatNum2);
 //		}
-		System.out.println(concatNum1);
-		System.out.println(concatNum2);
+
+//		no. 181842
+//		System.out.println("비교할 두 문자열을 입력해 주세요: ");
+//		String str1 = sc.nextLine();
+//		String str2 = sc.nextLine();
+//		
+//		if (str1.contains(str2)) {
+//			System.out.println(true);
+//		} else {
+//			System.out.println(false);
+//		}
+
+//		no. 181843
+//		System.out.println("비교할 두 문자열을 입력해 주세요: ");
+//		String str1 = sc.nextLine();
+//		String str2 = sc.nextLine();
+//		
+//		if (str1.contains(str2)) {
+//			System.out.println(1);
+//		} else {
+//			System.out.println(0);
+//		}
+
+//		no. 181845
+//		System.out.println("정수를 입력하시오: ");
+//		int num1 = sc.nextInt();
+//		System.out.println(Integer.toString(num1));
+
+//		no. 181848
+//		System.out.println("정수를 입력하시오: ");
+//		String str1 = sc.nextLine();
+//		System.out.println(Integer.parseInt(str1));
+
+//		no. 181847
+//		System.out.println("정수를 입력하시오: ");
+//		String str1 = sc.nextLine();
+//		System.out.println(Integer.parseInt(str1));
+
+//		no. 181849
+//		System.out.println("정수를 입력하시오: ");
+//		String str1 = sc.nextLine();
+//		String[] strArray = str1.split("");
+//		int sum = 0;
+//		for (int i = 0; i < strArray.length; i++) {
+//			sum += Integer.parseInt(strArray[i]);
+//		}
+//		System.out.println(sum);
+
+//		no. 181876
+//		System.out.println("영어 대소문자를 섞어서 입력하세요: ");
+//		String str1 = sc.nextLine();
+//		System.out.println(str1.toLowerCase());
+
+//		no. 181878
+//		System.out.println("영어 대소문자를 섞어서 입력하세요: ");
+//		String str1 = sc.nextLine();
+//		String str2 = sc.nextLine();
+//		if (str1.toLowerCase().contains(str2.toLowerCase())) {
+//			System.out.println(1);
+//		} else {
+//			System.out.println(0);
+//		}
+
+//		no. 181875
+		String str1 = sc.nextLine();
+		String[] strArray = str1.split(" ");
+
+		for (int i = 0; i < strArray.length; i++) {
+			if (i % 2 == 1) {
+				System.out.print(strArray[i].toUpperCase() + " ");
+			} else {
+				System.out.print(strArray[i].toLowerCase() + " ");
+			}
+		}
+
 	}
 }
