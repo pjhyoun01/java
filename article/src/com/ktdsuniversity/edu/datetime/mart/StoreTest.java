8d1f06031fb6d8549644684ac59906eb1cf1d61e
pjhyoun01 <pjhyoun01@gmail.com>
2026-02-04 19:55:54 +0900

    끝

--- article/src/com/ktdsuniversity/edu/datetime/mart/StoreTest.java
+++ article/src/com/ktdsuniversity/edu/datetime/mart/StoreTest.java
@@ -1,5 +1,7 @@
 package com.ktdsuniversity.edu.datetime.mart;
 
+import java.util.Iterator;
+
 public class StoreTest {
 	public static void main(String[] args) {
 		
@@ -15,18 +17,9 @@ public class StoreTest {
 		store.add(new Item("맛없는우유", "2026-02-03"));
 		store.add(new Item("맛있는우유", "2026-02-07"));
 		
-		store.sell(0);
-		store.sell(1);
-		store.sell(2);
-		store.sell(3);
-		store.sell(4);
-		store.sell(5);
-		store.sell(6);
-		store.sell(7);
-		store.sell(8);
-		store.sell(9);
-		store.sell(10);
-		store.sell(11);
+		for (int i = 0; i < 12; i++) {
+			store.sell(i);
+		}
 		
 		/*
 		상품명: 우유, 소비기한: 2026-02-04
@@ -36,13 +29,13 @@ public class StoreTest {
 		가능한 빨리 드세요
 		
 		상품명: 남양우유, 소비기한: 2026-02-07
-		7일 까지 드세요
+		2월 7일 까지 드세요
 		
 		상품명: 딸기우유, 소비기한: 2026-02-05
 		가능한 빨리 드세요
 		
 		상품명: 초코우유, 소비기한: 2026-02-08
-		8일 까지 드세요
+		2월 8일 까지 드세요
 		
 		상품명: 메론우유, 소비기한: 2026-02-04
 		오늘까지 드세요
@@ -57,7 +50,11 @@ public class StoreTest {
 		소비기한이 지나 판매하지 않습니다
 		
 		상품명: 맛있는우유, 소비기한: 2026-02-07
-		7일 까지 드세요
+		2월 7일 까지 드세요
+		
+		그런 상품은 없습니다. 상품번호를 확인하고 입력해주세요.
+		
+		그런 상품은 없습니다. 상품번호를 확인하고 입력해주세요.
 		 */
 	}
 	
